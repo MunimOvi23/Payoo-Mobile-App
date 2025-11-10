00cfc239d250693202aecc342684081adc191aa5
Munim Bin Muquith <munim.bin.muquith@g.bracu.ac.bd>
2025-11-11 02:13:03 +0600

    Latest updated5 commit

--- Script1.js
+++ Script1.js
@@ -513,5 +513,5 @@ document.getElementById("transaction").addEventListener("click", function(){
 
 // Logout
 document.getElementById("logout").addEventListener("click", function(){
-    window.location.href = "/Payoo-Mobile-App/index.html";
+    window.location.href = "https://munimovi23.github.io/Payoo-Mobile-App/";
 })
